f373dc7d4f4301148341675b24b2dff74e02bca0
sebastien verbist <sebastien.verbist@hotmail.com>
2022-10-07 16:42:52 +0200

    :art: loading page

--- app/javascript/controllers/menu_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = ["active"]
-  connect() {
-    this.element.textContent = "Hello World!"
-  }
-}
